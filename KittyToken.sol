6b3c5be7a065acbd127753dde29c5bb487339b36
Tanvir Rahaman <info@quasarchain.com>
2021-06-11 05:14:56 +0600

    Update contract DS to Solid BEP20

--- KittyToken.sol
+++ KittyToken.sol
@@ -1,291 +1,200 @@
+// SPDX-License-Identifier: MIT
+
 
 pragma solidity = 0.5.17;
 
-contract KITTYMath {
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x, "KITTY-math-add-overflow");
-    }
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, "KITTY-math-sub-underflow");
-    }
-    function mul(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x, "KITTY-math-mul-overflow");
-    }
-
-    function div(uint x, uint y) internal pure returns (uint z) {
-        require(y > 0, "KITTY-math-div-overflow");
-        z = x / y;
-    }
-
-    function min(uint x, uint y) internal pure returns (uint z) {
-        return x <= y ? x : y;
-    }
-    function max(uint x, uint y) internal pure returns (uint z) {
-        return x >= y ? x : y;
-    }
 
 
-    uint constant WAD = 10 ** 18;
+interface IBEP20 {
 
-    function wdiv(uint x, uint y) internal pure returns (uint z) {
-        z = add(mul(x, WAD), y / 2) / y;
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+
+library SafeMath {
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
     }
 
-    function pow(uint256 base, uint256 exponent) public pure returns (uint256) {
-        if (exponent == 0) {
-            return 1;
-        }
-        else if (exponent == 1) {
-            return base;
-        }
-        else if (base == 0 && exponent != 0) {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+
+         if (a == 0) {
             return 0;
         }
-        else {
-            uint256 z = base;
-            for (uint256 i = 1; i < exponent; i++)
-                z = mul(z, base);
-            return z;
-        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0, "SafeMath: modulo by zero");
+        return a % b;
     }
 }
 
-contract KITTYAuthEvents {
-    event LogSetAuthority (address indexed authority);
-    event LogSetOwner     (address indexed owner);
-}
 
-contract KITTYAuth is KITTYAuthEvents {
-    address      public  authority;
-    address      public  owner;
 
-    constructor() public {
-        owner = msg.sender;
-        emit LogSetOwner(msg.sender);
+contract BEP20 is IBEP20 {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 
-    function setOwner(address owner_)
-        public
-        onlyOwner
-    {
-        require(owner_ != address(0), "invalid owner address");
-        owner = owner_;
-        emit LogSetOwner(owner);
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
     }
 
-    function setAuthority(address authority_)
-        public
-        onlyOwner
-    {
-        authority = authority_;
-        emit LogSetAuthority(address(authority));
-    }
 
-    modifier auth {
-        require(isAuthorized(msg.sender), "KITTY-auth-unauthorized");
-        _;
-    }
-
-    modifier onlyOwner {
-        require(isOwner(msg.sender), "KITTY-auth-non-owner");
-        _;
-    }
-
-    function isOwner(address src) public view returns (bool) {
-        return bool(src == owner);
-    }
-
-    function isAuthorized(address src) internal view returns (bool) {
-        if (src == address(this)) {
-            return true;
-        } else if (src == owner) {
-            return true;
-        } else if (authority == address(0)) {
-            return false;
-        } else if (src == authority) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-contract KITTYNote {
-    event LogNote(
-        bytes4   indexed  sig,
-        address  indexed  guy,
-        bytes32  indexed  foo,
-        bytes32  indexed  bar,
-        uint256           wad,
-        bytes             fax
-    ) anonymous;
-
-    modifier note {
-        bytes32 foo;
-        bytes32 bar;
-        uint256 wad;
-
-        assembly {
-            foo := calldataload(4)
-            bar := calldataload(36)
-            wad := callvalue
-        }
-
-        emit LogNote(msg.sig, msg.sender, foo, bar, wad, msg.data);
-
-        _;
-    }
-}
-
-contract KITTYStop is KITTYNote, KITTYAuth, KITTYMath {
-    bool public stopped;
-
-    modifier stoppable {
-        require(!stopped, "KITTY-stop-is-stopped");
-        _;
-    }
-    function stop() public onlyOwner note {
-        stopped = true;
-    }
-    function start() public onlyOwner note {
-        stopped = false;
-    }
-}
-
-contract BEP20Events {
-    event Approval(address indexed src, address indexed guy, uint wad);
-    event Transfer(address indexed src, address indexed dst, uint wad);
-}
-
-contract BEP20 is BEP20Events {
-    function totalSupply() public view returns (uint);
-    function balanceOf(address guy) public view returns (uint);
-    function allowance(address src, address guy) public view returns (uint);
-
-    function approve(address guy, uint wad) public returns (bool);
-    function transfer(address dst, uint wad) public returns (bool);
-    function transferFrom(address src, address dst, uint wad) public returns (bool);
-}
-
-contract KITTYTokenBase is BEP20, KITTYMath {
-    uint256                                            _supply;
-    mapping (address => uint256)                       _balances;
-    mapping (address => mapping (address => uint256))  _approvals;
-
-    constructor(uint supply) public {
-        _supply = supply;
-    }
-
-    function totalSupply() public view returns (uint) {
-        return _supply;
-    }
-    function balanceOf(address src) public view returns (uint) {
-        return _balances[src];
-    }
-    function allowance(address src, address guy) public view returns (uint) {
-        return _approvals[src][guy];
-    }
-
-    function transfer(address dst, uint wad) public returns (bool) {
-        return transferFrom(msg.sender, dst, wad);
-    }
-
-    function transferFrom(address src, address dst, uint wad)
-        public
-        returns (bool)
-    {
-        if (src != msg.sender) {
-            require(_approvals[src][msg.sender] >= wad, "KITTY-token-insufficient-approval");
-            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
-        }
-
-        require(_balances[src] >= wad, "KITTY-token-insufficient-balance");
-        _balances[src] = sub(_balances[src], wad);
-        _balances[dst] = add(_balances[dst], wad);
-
-        emit Transfer(src, dst, wad);
-
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function approve(address guy, uint wad) public returns (bool) {
-        _approvals[msg.sender][guy] = wad;
 
-        emit Approval(msg.sender, guy, wad);
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowances[owner][spender];
+    }
 
+    function approve(address spender, uint256 value) public returns (bool) {
+        _approve(msg.sender, spender, value);
         return true;
+    }
+
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount));
+        return true;
+    }
+
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender].add(addedValue));
+        return true;
+    }
+
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender].sub(subtractedValue));
+        return true;
+    }
+
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "BEP20: transfer from the zero address");
+        require(recipient != address(0), "BEP20: transfer to the zero address");
+
+        _balances[sender] = _balances[sender].sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "BEP20: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 value) internal {
+        require(account != address(0), "BEP20: burn from the zero address");
+
+        _totalSupply = _totalSupply.sub(value);
+        _balances[account] = _balances[account].sub(value);
+        emit Transfer(account, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint256 value) internal {
+        require(owner != address(0), "BEP20: approve from the zero address");
+        require(spender != address(0), "BEP20: approve to the zero address");
+
+        _allowances[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+
+    function _burnFrom(address account, uint256 amount) internal {
+        _burn(account, amount);
+        _approve(account, msg.sender, _allowances[account][msg.sender].sub(amount));
     }
 }
 
-contract KittyToken is KITTYTokenBase(0), KITTYStop {
-
-    bytes32  public  name ;
-    bytes32  public  symbol;
-    uint256  public  decimals = 18;
-    uint256  public  supply;
-
-    constructor(bytes32 name_ , bytes32 symbol_) public {
-        symbol = symbol_;
-        name = name_;
-        }
 
 
 
-    function approvex(address guy) public stoppable returns (bool) {
-        return super.approve(guy, uint(-1));
+contract KittyToken is BEP20 {
+
+    bytes32 private _name;
+    bytes32 private _symbol;
+    uint8 private _decimals = 18;
+
+    constructor(bytes32 name, bytes32 symbol, uint256 totalSupply, address payable feeReceiver, address tokenOwnerAddress) public payable {
+      _name = name;
+      _symbol = symbol;
+
+
+     _mint(tokenOwnerAddress, totalSupply);
+      feeReceiver.transfer(msg.value);
     }
 
-    function approve(address guy, uint wad) public stoppable returns (bool) {
-        require(_approvals[msg.sender][guy] == 0 || wad == 0); //take care of re-approve.
-        return super.approve(guy, wad);
+    function burn(uint256 value) public {
+      _burn(msg.sender, value);
     }
 
-    function transferFrom(address src, address dst, uint wad)
-        public
-        stoppable
-        returns (bool)
-    {
-        if (src != msg.sender && _approvals[src][msg.sender] != uint(-1)) {
-            require(_approvals[src][msg.sender] >= wad, "KITTY-token-insufficient-approval");
-            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
-        }
-
-        require(_balances[src] >= wad, "KITTY-token-insufficient-balance");
-        _balances[src] = sub(_balances[src], wad);
-        _balances[dst] = add(_balances[dst], wad);
-
-        emit Transfer(src, dst, wad);
-
-        return true;
+    function name() public view returns (bytes32) {
+      return _name;
     }
 
-    function mint(address guy, uint wad) public auth stoppable {
-        _mint(guy, wad);
+   function symbol() public view returns (bytes32) {
+      return _symbol;
     }
 
-    function burn(address guy, uint wad) public auth stoppable {
-        _burn(guy, wad);
-    }
-
-     function _mint(address guy, uint wad) internal {
-        require(guy != address(0) && supply != 1000000000000, "KITTY-token-mint: mint to the zero address");
-
-      _balances[guy] = add(_balances[guy], wad);
-        _supply = add(_supply, wad);
-        emit Transfer(address(0), guy, wad);
-    }
-
-    function _burn(address guy, uint wad) internal {
-        require(guy != address(0), "KITTY-token-mint: mint to the zero address");
-        require(_balances[guy] >= wad, "KITTY-token-insufficient-balance");
-
-        if (guy != msg.sender && _approvals[guy][msg.sender] != uint(-1)) {
-            require(_approvals[guy][msg.sender] >= wad, "KITTY-token-insufficient-approval");
-            _approvals[guy][msg.sender] = sub(_approvals[guy][msg.sender], wad);
-        }
-
-        _balances[guy] = sub(_balances[guy], wad);
-        _supply = sub(_supply, wad);
-        emit Transfer(guy, address(0), wad);
+  function decimals() public view returns (uint8) {
+      return _decimals;
     }
 }
